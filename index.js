0959dde5692e800bb0b8611db8013983e291a3b8
Teofil Camarasu <teofilcamarasu@gmail.com>
2019-07-17 17:57:17 +0100

    Fix sync issue

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 
 state = "";
+last_update = 0;
 start = 0;
 end = 0;
 
@@ -15,10 +16,11 @@ app.get('/', function(req,res) {
 });
 
 io.on('connection', function(socket) {
-	socket.emit('replace', state, Date.now());
+	socket.emit('replace', state, last_update);
 	socket.emit('select', start, end);
 	socket.on('replace', function(txt,time) {
 		state = txt;
+    last_update = time;
 		socket.broadcast.emit("replace", txt, time)
 
 	});
